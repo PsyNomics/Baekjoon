38f34bfca21ae26842251ae30c7248822d8b1dac
PsyNomics <psynomics93@gmail.com>
2021-02-10 16:30:59 +0900

    알파벳

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -1,35 +1,32 @@
 package Test;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 public class Main {
-	public static void main(String[] args) throws IOException{
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+
+
+		String alp = "abcdefghijklmnopqrstuvwxyz";
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = sc.nextInt();
-		int[][] arr = new int[N][];
-		
-		for(int i=0;i<arr.length;i++) {
-			
-			StringTokenizer st = new StringTokenizer(br.readLine()," ");
-			int Num = Integer.parseInt(st.nextToken());
-			arr = new int[i][Num];
-			
-			for(int j=0;j<arr[i].length;j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+		String word = br.readLine();
+		String[] wordArray = word.split("");
+		String[] alpArray = alp.split("");
+		boolean check;
+
+		for(int j=0;j<alpArray.length;j++) {
+			check = false;
+			for(int i=0;i<wordArray.length;i++) {
+				if(wordArray[i].equals(alpArray[j])) {
+					System.out.print(i+" ");
+					check=true;
+				}
 			}
-		}
-		
-		for(int i=0;i<arr.length;i++) {
-			int sum = 0;
-			for(int j=0;j<arr[i].length;j++) {
-				sum+=arr[i][j];
+			if(check != true) {
+				System.out.print(-1+" ");
 			}
-			double avg = sum/arr[i][0];
-			System.out.println(avg);
 		}
 	}
+	//baekjoon	
 }
