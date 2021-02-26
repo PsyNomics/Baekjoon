aa4590166f737d0b6aae30aa32b2dacf0200ffb2
PsyNomics <psynomics93@gmail.com>
2021-02-26 15:23:18 +0900

    study word test 1157

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -3,36 +3,41 @@ package Test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int Num;
-		Num = Integer.parseInt(br.readLine());
+		String str2 = br.readLine().toUpperCase();
+		String[] arr = str2.split("");
 		
-		for(int i=0;i<Num;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int count = Integer.parseInt(st.nextToken());
-			
-			
-			String alp = st.nextToken();
-			StringTokenizer st2 = new StringTokenizer(alp);
-			String[] word = new String[st2.countTokens()];
-			int j = 0;
-			//배열 만들기
-			
-			while(st2.hasMoreTokens()) {
-				word[j] = st2.nextToken();
-				
-				for(int k=0;k<count;k++) {
-					System.out.print(word[j]);
-				}
-				j++;
-			}
-			System.out.println();
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
 		}
+		//-------------------------
+		char[] alp = new char[26];
+		alp[0]=65;
+		
+		for(int i=0;i<alp.length-1;i++) {
+			alp[i+1]= (char)(alp[i]+1);
+		}
+		
+		int[] count = new int[alp.length]; //중복 횟수 카운트용
+		
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<alp.length;j++) {// char 숫자값
+				
+				if(arr[i].equals(String.valueOf(alp[j]))) {
+					count[j]+=1;
+				}
+			}
+		}
+		
+		for(int i=0;i<alp.length;i++) {
+			System.out.print(count[i]);
+		}
+		
+		//배열중 수가 가장 많은 값을 구하기 - 개수가 같으면 '?'출력
+		
 	}
 }
---
+++ src/Test/Test.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Test {
+	long sum(int[] a) {
+		int N=0;
+		for(int i=0;i<a.length;i++) {
+			N += a[i];
+		}
+		return N;
+	}
+
+	public static void main(String[] args) {
+		baekjoon
+	}
+}
